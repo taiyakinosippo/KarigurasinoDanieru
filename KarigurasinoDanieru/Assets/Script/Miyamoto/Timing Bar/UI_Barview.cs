82974c2180bdc0a1abe12feb9aca76f17bce0994
MiyamotoSouma <souchan20050609panda@icloud.com>
2026-04-16 10:13:10 +0900

    タイミングバー完成(演出なし)
    演出はどんなのがいいのかわからないため一旦放置
    -script--------------------------------------
    JudgeType
    enumでスコアの管理
    Timeing_Bar_Logic
    タイミングバーの設定はここのinspectorでだいたい設定できるはず、タイミングバーの司令塔
    Timing_Bar_System
    タイミングバーのシステムこれは判定や移動の仕方を担ってる
    objectにアタッチする必要がない
    UI_Barview
    barの移動の処理はここでやっている
    UI_Judge
    TExtに表示させる関係はここでやっている
    また音や、スコア計算もここ
    -Aseet------------------------------------
    Timing bar追加くらい

---
+++ KarigurasinoDanieru/Assets/Script/Miyamoto/Timing Bar/UI_Barview.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class UI_Barview : MonoBehaviour
+{
+    private RectTransform bar_transpos;
+    public void Awake()
+    {
+        bar_transpos = GetComponent<RectTransform>();
+    }
+    public void barPosition(float move, float _maxwindth)
+    {
+        float windth = _maxwindth * (move / 100f);
+        bar_transpos.anchoredPosition = new Vector2(windth, bar_transpos.anchoredPosition.y);
+    }
+
+    public void HideBar()
+    {
+        bar_transpos.gameObject.SetActive(false);
+    }
+
+    public void ShowBar()
+    {
+        bar_transpos.gameObject.SetActive(true);
+        bar_transpos.anchoredPosition = new Vector2(0, bar_transpos.anchoredPosition.y);
+    }
+}
---
+++ KarigurasinoDanieru/Assets/Script/Miyamoto/Timing Bar/UI_Judge.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+public class UI_Judge : MonoBehaviour
+{
+    public TextMeshProUGUI judge_text;
+    public TextMeshProUGUI score_text;
+    public Timeing_Bar_Logic timing_bar_Logic;
+    int score = 0;
+
+    public void SetScoreText(JudgeType result)
+    {
+        switch (result)
+        {
+            case JudgeType.Miss:
+                judge_text.text = "Miss";
+                judge_text.color = Color.red;
+                AudioSourceManager.instance.PlaySE(SEType.missSE);
+                score += timing_bar_Logic.miss_score;
+                break;
+                
+            case JudgeType.Good:
+                judge_text.text = "Good";
+                judge_text.color = Color.green;
+                AudioSourceManager.instance.PlaySE(SEType.goodSE);
+                score += timing_bar_Logic.good_score;
+
+                break;
+
+            case JudgeType.Great:
+                judge_text.text = "Great";
+                judge_text.color = Color.yellow;
+                AudioSourceManager.instance.PlaySE(SEType.greatSE);
+                score += timing_bar_Logic.great_score;
+                break;
+            case JudgeType.Perfect:
+                judge_text.text = "Perfect";
+                judge_text.color = Color.orange;
+                AudioSourceManager.instance.PlaySE(SEType.perfectSE);
+                score += timing_bar_Logic.perfect_score;
+                break;
+        }
+        ScoreManager.instance.AddScore(score);
+        score = 0;
+        score_text.text = "Score: " + ScoreManager.instance.GetScore();
+    }
+}
