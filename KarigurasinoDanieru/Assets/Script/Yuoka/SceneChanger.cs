6165e460be54b005cdda43a85c1c717cc3784859
Yuoka <kmm2659370@stu.o-hara.ac.jp>
2026-05-22 16:37:28 +0900

    ロケットが飛び終わってからリザルトを表示するまでの待機時間を設定できる機能を追加
    ResultManager：インスペクターで設定した時間、リザルトの表示を待つ
    normalDelay：通常の演出の待機時間
    highScoreDelay：惑星に着地する演出の待機時間

--- KarigurasinoDanieru/Assets/Script/Yuoka/SceneChanger.cs
+++ KarigurasinoDanieru/Assets/Script/Yuoka/SceneChanger.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
     public void ChangeScene(string sceneName)
     {
