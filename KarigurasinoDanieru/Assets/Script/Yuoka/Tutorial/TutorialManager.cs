344f7e8e3f3d22e4fd583303ef6e0208310b8f1d
Yuoka <kmm2659370@stu.o-hara.ac.jp>
2026-06-09 09:50:26 +0900

    チュートリアルパネルを作成

---
+++ KarigurasinoDanieru/Assets/Script/Yuoka/Tutorial/TutorialManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] tutorialImages;
+
+    private int currentIndex = 0;
+    private bool canClick = false;
+
+    IEnumerator Start()
+    {
+        // 1•b‘Ò‚Â
+        yield return new WaitForSecondsRealtime(1f);
+
+        Time.timeScale = 0f;
+
+        for (int i = 0; i < tutorialImages.Length; i++)
+        {
+            tutorialImages[i].SetActive(i == 0);
+        }
+
+        canClick = true;
+    }
+
+    void Update()
+    {
+        if (!canClick) return;
+
+        if (Mouse.current != null &&
+            Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            NextTutorial();
+        }
+    }
+
+    void NextTutorial()
+    {
+        tutorialImages[currentIndex].SetActive(false);
+
+        currentIndex++;
+
+        if (currentIndex >= tutorialImages.Length)
+        {
+            EndTutorial();
+            return;
+        }
+
+        tutorialImages[currentIndex].SetActive(true);
+    }
+
+    void EndTutorial()
+    {
+        Time.timeScale = 1f;
+        gameObject.SetActive(false);
+    }
+}
